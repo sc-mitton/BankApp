ded634748532e65bfa818a231ffe576896cb6fa3
Spencer Mitton <smitton.byu@gmail.com>
2022-10-17 08:56:25 -0400

    Nothing major changed

--- src/main/java/com/bankapp/Main.java
+++ src/main/java/com/bankapp/Main.java
@@ -1,8 +1,13 @@
 package com.bankapp;
 
+import java.io.IOException;
+
+import util.LoggingUtil;
+
 // Driver program for the Bank application
 public class Main {
   public static void main(String[] args) {
+
     Screen screen = Screen.getInstance();
     screen.run();
   }
